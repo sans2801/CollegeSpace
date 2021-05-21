b657a48c0c1bac1f812bd8c53ab63b7b70d9182b
Akshay Bakshi <apbakshi@hotmail.com>
2021-05-21 11:32:11 +0530

    resolved conflicts in pagination

--- react-client/src/Components/Internships/Pagination.js
+++ react-client/src/Components/Internships/Pagination.js
@@ -33,40 +33,42 @@ const Pagination = ({ data }) => {
     return blogs.slice(startIndex, endIndex);
   };
 
-  const getPaginatedData = () => {
-    const startIndex = currentPage * dataLimit - dataLimit;
-    const endIndex = startIndex + dataLimit;
-    return blogs.slice(startIndex, endIndex);
-  };
-
   const goToNextPage = () => {
     setCurrentPage((page) => page + 1);
-  }
+  };
 
   const goToPreviousPage = () => {
     setCurrentPage((page) => page - 1);
-  }
+  };
 
   return (
     <div>
       <div className="dataContainer">
         {pending && <div>Fetching blogs...</div>}
-        {blogs != null && getPaginatedData().map((singleblog, idx) => (
-          <div className="card mb-4" key={idx}>
-            <div className="card-body">
-              <h2 className="card-title">{singleblog.title}</h2>
-              <p className="card-text">{`${singleblog.text.slice(0, 200)}...`}</p>
-              <Link className="btn btn-primary" to={{
-                pathname: `/singlepost/${singleblog.id}`,
-                blog: singleblog,
-              }}>Read More →
-                            </Link>
+        {blogs != null &&
+          getPaginatedData().map((singleblog, idx) => (
+            <div className="card mb-4" key={idx}>
+              <div className="card-body">
+                <h2 className="card-title">{singleblog.title}</h2>
+                <p className="card-text">{`${singleblog.text.slice(
+                  0,
+                  200
+                )}...`}</p>
+                <Link
+                  className="btn btn-primary"
+                  to={{
+                    pathname: `/singlepost/${singleblog.id}`,
+                    blog: singleblog,
+                  }}
+                >
+                  Read More →
+                </Link>
+              </div>
+              <div className="card-footer text-muted">
+                {`Posted on January 1, 2021 by  ${singleblog.author}`}
+              </div>
             </div>
-            <div className="card-footer text-muted">
-              {`Posted on January 1, 2021 by  ${singleblog.author}`}
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
       <ul className="pagination justify-content-center mb-4">
         <li className="page-item">
@@ -77,7 +79,7 @@ const Pagination = ({ data }) => {
             disabled={currentPage === 1 ? true : false}
           >
             ← Older
-                    </Button>
+          </Button>
         </li>
         <li className="page-item">
           <Button
@@ -87,11 +89,11 @@ const Pagination = ({ data }) => {
             disabled={currentPage === pages ? true : false}
           >
             Newer →
-                    </Button>
+          </Button>
         </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default Pagination;
